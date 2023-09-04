954f4be1b4c5911c029904e9276640f658a4b987
lisal00 <143776518+lisal00@users.noreply.github.com>
2023-09-04 18:08:35 -0500

    Add files via upload

--- Question 2/main2.cpp
+++ Question 2/main2.cpp
@@ -1,10 +1,7 @@
-#include "header2.h"
-#include "header2.cpp"  //remove this later
+#include "array2.h"
 #include <iostream>
 #include <fstream>
 #include <stdexcept>
-#include <ios> //remove?
-
 using namespace std;
 
 int main(){
@@ -15,58 +12,57 @@ int main(){
     int new_value = 0;
     int end_value = 0;
 
-    int x[x_size];
+    int x[x_size]; //initialize array x
 
     read_array(x);
-/**
-    /**tests check_array function
-    cout << "\nEnter in an integer to check if it exists in the array." << endl;
+
+    /**tests check_array function*/
+    cout << "\n1) Enter in an integer to check if it exists in the array." << endl;
     cin >> target;
     value = check_array(x, target, x_size);
     if (value != -1)
         cout << "Value " << target << " exits at index " << value << ".";
     else
-        cout << "Value " << target << "was not found.";
-*/
+        cout << "Value " << target << " was not found.";
+
 
     /**tests set_value function*/
-    cout << "\nEnter an index 0-149 you wish to modify.\n";
-    
+    cout << "\n2) Enter an index 0-149 you wish to modify.\n";
+    cin.exceptions(ios_base::failbit);
     try{
-        cin.exceptions(ios_base::failbit);
         cin >> index;
-        
-        //cin >> index;
-        /**
-        if (index < 0 || index > 150){
-            throw index;
+        if (index < 0 || index > 150){ //checks to see if input is in bounds
+            throw 0;
         }
-        */
+        cout << "Enter in the value you wish to replace it with.\n";
+        cin >> new_value;
     }
     catch(ios_base::failure& ex){
-        cerr << "womomom " << ex.what();
+        cerr << "Invalid input. Please enter a whole number.";
         abort();
     }
-
-    cout << "Enter in the value you wish to replace with.\n";
-    cin >> new_value;
+    catch(int ex2){
+        cout << "Out of range from array.";
+        abort();
+    }
+    catch(...){
+        cout << "Undefined exception occured.";
+        abort();
+    }
     set_value(x, index, new_value);
-    
+
     
     /**tests add_value function*/
-    cout << "\nWhat value would you like to add to the end of the array?\n";
+    cout << "\n3) What value would you like to add to the end of the array?\n";
     cin >> end_value;
     add_value(x, end_value);
     cout << "The value at index 100 is now " << x[100] << ".\n";
 
-
-
-/**
-    /**tests remove_value function
-    cout << "Enter in an index 0-149 you wish to remove.\n";
+    /**tests remove_value function*/
+    cout << "4) Enter in an index 0-149 you wish to remove.\n";
     cin >> index;
     remove_value(x, index, x_size);
     cout << "Index " << index << " is now removed.";
-*/
+
     return 0;
 }
