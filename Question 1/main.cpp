01b600808055b68cf7955591fb8008feb85ff56e
lisal00 <143776518+lisal00@users.noreply.github.com>
2023-09-09 18:36:48 -0500

    Update main.cpp

--- Question 1/main.cpp
+++ Question 1/main.cpp
@@ -17,29 +17,29 @@ int main(){
     read_array(x);
 
     /**tests check_array function*/
-    cout << "\n1) Enter in an integer to check if it exists in the array." << endl;
+    cout << "1) Enter in an integer to check if it exists in the array: " ;
     cin >> target;
-    value = check_array(x, target, x_size);
+    value = check_array(x, target, x_size); 
     if (value != -1)
-        cout << "Value " << target << " exits at index " << value << ".";
+        cout << "Value " << target << " exits at index " << value << ".\n";
     else
-        cout << "Value " << target << " was not found.";
+        cout << "Value " << target << " was not found.\n";
 
     /**tests set_value function*/
-    cout << "\n2) Enter an index 0-149 you wish to modify.\n";
+    cout << "\n2) Enter an index 0-149 you wish to modify: ";
     cin >> index;
-    cout << "Enter in the value you wish to replace it with.\n";
+    cout << "Enter in the value you wish to replace it with: ";
     cin >> new_value;
     set_value(x, index, new_value);
 
     /**tests add_value function*/
-    cout << "\n3) What value would you like to add to the end of the array?\n";
+    cout << "\n3) What value would you like to add to the end of the array? ";
     cin >> end_value;
     add_value(x, end_value);
     cout << "The value at index 100 is now " << x[100] << ".\n";
 
     /**tests remove_value function*/
-    cout << "4) Enter in an index 0-149 you wish to remove.\n";
+    cout << "\n4) Enter in an index 0-149 you wish to remove: ";
     cin >> index;
     remove_value(x, index, x_size);
     cout << "Index " << index << " is now removed.";
