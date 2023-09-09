75a442f51ff313975df09902adae84d4943958b4
lisal00 <143776518+lisal00@users.noreply.github.com>
2023-09-09 17:55:49 -0500

    Update main2.cpp

--- Question 2/main2.cpp
+++ Question 2/main2.cpp
@@ -1,6 +1,7 @@
 #include "array2.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <stdexcept>
 using namespace std;
 
@@ -11,13 +12,16 @@ int main(){
     int index = 0;
     int new_value = 0;
     int end_value = 0;
+    string new_value_str;
+    string index_str;
+    string end_value_str;
 
     int x[x_size]; //initialize array x
 
     read_array(x);
 
     /**tests check_array function*/
-    cout << "\n1) Enter in an integer to check if it exists in the array." << endl;
+    cout << "\n1) Enter in an integer to check if it exists in the array: ";
     cin >> target;
     value = check_array(x, target, x_size);
     if (value != -1)
@@ -27,41 +31,49 @@ int main(){
 
 
     /**tests set_value function*/
-    cout << "\n2) Enter an index 0-149 you wish to modify.\n";
-    cin.exceptions(ios_base::failbit);
-    try{
-        cin >> index;
-        if (index < 0 || index > 149){ //checks to see if input is in bounds
-            throw 0;
+    cout << "\n2) Enter an index 0-149 you wish to modify: ";
+    cin >> index_str;
+     try{
+        istringstream index_stream(index_str);
+        if (!(index_stream >> index) || !index_stream.eof() || index < 0 || index > 149){ //checks to see if str can turn into int & if in bounds
+            throw invalid_argument("Invalid input for index."); //else, will throw exception
         }
-        cout << "Enter in the value you wish to replace it with.\n";
-        cin >> new_value;
+
+        cout << "Enter in the value you wish to replace it with: ";
+        cin >> new_value_str;
+        istringstream new_value_stream(new_value_str); 
+        if (!(new_value_stream >> new_value) || !new_value_stream.eof()){//checks to see if str can turn into int
+            throw invalid_argument("Invalid input for new_value."); //if not, will throw exception
+        }
     }
-    catch(ios_base::failure& ex){
-        cerr << "Invalid input. Please enter a whole number.";
-        abort();
-    }
-    catch(int ex2){
-        cout << "Out of range from array.";
-        abort();
-    }
-    catch(...){
-        cout << "Undefined exception occured.";
+    catch (const invalid_argument& ex) {
+        cerr << "Error: " << ex.what() << endl;
         abort();
     }
     set_value(x, index, new_value);
 
-    
+
     /**tests add_value function*/
-    cout << "\n3) What value would you like to add to the end of the array?\n";
-    cin >> end_value;
+    cout << "\n3) What value would you like to add to the end of the array? ";
+    try {
+        cin >> end_value_str;
+        istringstream end_value_stream(end_value_str); 
+        if (!(end_value_stream >> end_value) || !end_value_stream.eof()){//checks to see if str can turn into int
+            throw invalid_argument("Invalid input for end_value."); //if not, will throw exception
+        }
     add_value(x, end_value);
-    cout << "The value at index 100 is now " << x[100] << ".\n";
+    }
+    catch(const invalid_argument& ex){
+        cerr << "\nError: "<< ex.what();
+        abort();
+    }
+    cout << "The value at index 100 is now " << x[100] << ".";
+
 
     /**tests remove_value function*/
-    cout << "4) Enter in an index 0-149 you wish to remove.\n";
+    cout << "\n4) Enter in an index 0-149 you wish to remove: ";
     cin >> index;
-    remove_value(x, index, x_size);
+    remove_value(x, index, x_size); 
     cout << "Index " << index << " is now removed.";
 
     return 0;
