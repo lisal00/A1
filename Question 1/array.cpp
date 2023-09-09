284c3fcdb47e7877467191668c541111fa197963
lisal00 <143776518+lisal00@users.noreply.github.com>
2023-09-09 18:30:34 -0500

    Update array.cpp

--- Question 1/array.cpp
+++ Question 1/array.cpp
@@ -1,4 +1,4 @@
-#include "array.h"
+#include "array2.h"
 #include <fstream>
 #include <iostream>
 
@@ -11,8 +11,8 @@ void read_array(int x[]){
     ifstream inFile;
     inFile.open("input.txt");
     
-    while (!inFile.eof()){ 
-        for(int i = 0; i < 100; i++){ //loads integers into the array until index 99
+    while (!inFile.eof()){
+        for(int i = 0; i < 100; i++){//loads integers into the array until index 99
             inFile >> x[i];
         }
         for(int i = 100; i < 150; i++){ //loads 0's into the rest of array
@@ -29,7 +29,7 @@ void read_array(int x[]){
     @return Index of target if found, -1 if not
 */
 int check_array(int x[], int target, int x_size){
-    for(int i = 0; i < x_size; i++){ 
+    for(int i = 0; i < x_size; i++){
         if(x[i] == target){
             return i;
         }   
@@ -45,15 +45,14 @@ int check_array(int x[], int target, int x_size){
 */
 void set_value(int x[], int index, int new_value){
     int old_value = 0;
-
-    old_value = x[index]; 
+    old_value = x[index];
     x[index] = new_value;
 
     cout << "The old value of index " << index 
-    << " was " << old_value << ". The new value is now " << x[index] << ".";
+    << " was " << old_value << ". The new value is now " << x[index] << ".\n";
 }
 
-/** Adds new integer to end of array
+/** If valid input, adds new integer to end of array. If not, aborts.
     @param x Array to modify
     @param end_value Integer to add to end of array
 */
@@ -70,9 +69,4 @@ void remove_value(int x[], int index, int x_size){
     for (int i = index; i < x_size - 1; i++){
         x[i] = x[i+1];
     }
-    /* testing and printing out array
-    for(int i = 0; i < 150; i++){
-            cout << x[i] << " ";
-        }
-*/
 }
